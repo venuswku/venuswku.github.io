1fd4529677727b3b8b687a9c62d4915e667223ca
Venus Ku <venuswku@gmail.com>
2020-12-25 22:34:32 -0800

    position divs on about page & added some padding to bottom of home and about pages

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,22 +5,18 @@ function About() {
     return (
         <div className="about">
             <p className="about-heading">About</p>
-            <div className="about-content">
-                <div className="purple-section">
-                    Currently, I'm a third-year undergraduate studying Computer Science at the University of California in Santa Cruz.
-                    I hope to develop software projects that can benefit people
-                    because programming gives a meaningful purpose to the computer and mobile applications that we use in our daily lives.
-                </div>
-                <div className="blue-section">
-                    Whenever possible, I like to incorporate digital art into my projects.
-                    It's a nice way to visually express emotions and interactively connect with the people who look at my work.
-                </div>
+            <div className="blue-section">
+                Currently, I'm a third-year undergraduate studying Computer Science at the University of California in Santa Cruz.
+                I hope to develop software projects that can benefit people
+                because programming gives a meaningful purpose to the computer and mobile applications that we use in our daily lives.
+            </div>
+            <div className="purple-section">
+                Whenever possible, I like to incorporate digital art into my projects.
+                It's a nice way to visually express emotions and interactively connect with the people who look at my work.
             </div>
             <p className="about-heading">Resume</p>
             <div className="resume-content">
-                <div className="blue-section">
-                    <p className="download">Download</p>
-                </div>
+                <div className="download">Download</div>
             </div>
         </div>
     );
