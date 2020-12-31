5b00521f48ffa2a00e48de448a7456e05091fc15
Venus Ku <venuswku@gmail.com>
2020-12-31 13:02:35 -0800

    put resume in about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
 import { ReactComponent as SeaLionBody } from '../../Assets/SeaLionBody.svg';
+import Resume from '../../Assets/VenusResume.pdf';
 
 function About() {
     const resumeRef = React.useRef();
@@ -23,10 +24,10 @@ function About() {
                 Whenever possible, I like to incorporate digital art into my projects.
                 It's a nice way to visually express emotions and interactively connect with the people who look at my work.
             </div>
-            <p className="heading">Resume</p>
-            <div className="resume-content" ref={resumeRef}>
-                <div className="download">Download</div>
-            </div>
+            <p className="heading" ref={resumeRef}>Resume</p>
+            <object data={Resume} type="application/pdf" width="100%" height="950px" className="resume">
+                alt: <a href={Resume} target="_blank" rel="noopener noreferrer">VenusResume.pdf</a>
+            </object>
         </div>
     );
 }
