19a6280afa7c91caa7a7dbba8de739be25cf6e20
Venus Ku <venuswku@gmail.com>
2020-12-31 14:06:46 -0800

    Updates

---
+++ precache-manifest.8ce9b9be091dd178f058f8639811060b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c76f275c12b72a32633a67d246e6fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bac7b5217e3e479e7773",
+    "url": "/static/css/main.24c0828c.chunk.css"
+  },
+  {
+    "revision": "03f2bdcc98d4332ac0cd",
+    "url": "/static/js/2.7e76c951.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7e76c951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac7b5217e3e479e7773",
+    "url": "/static/js/main.c44995aa.chunk.js"
+  },
+  {
+    "revision": "6ec1ab48d225aa86ebde",
+    "url": "/static/js/runtime-main.72037fc3.js"
+  },
+  {
+    "revision": "4546f6043dc3fd25b3a02c1feab6e970",
+    "url": "/static/media/Mom.4546f604.gif"
+  },
+  {
+    "revision": "7a5d44880d428a0d692eb918fcf4fb8f",
+    "url": "/static/media/OctopusBody.7a5d4488.svg"
+  },
+  {
+    "revision": "b41fd3b882d6f811f04746525e156e19",
+    "url": "/static/media/OctopusLeftTentacle.b41fd3b8.svg"
+  },
+  {
+    "revision": "1d849759bed62774b7b0eb26001e6f0a",
+    "url": "/static/media/OctopusRightTentacle.1d849759.svg"
+  },
+  {
+    "revision": "17daa41ef2a4b092fa0fb42aea5bf579",
+    "url": "/static/media/PersonalWebsite.17daa41e.gif"
+  },
+  {
+    "revision": "a5879c264eb0f8bd374218d07f941962",
+    "url": "/static/media/SeaLionBody.a5879c26.svg"
+  },
+  {
+    "revision": "1e0efe0dc31e0afacecf7d21abe06717",
+    "url": "/static/media/SeaLionTail.1e0efe0d.svg"
+  },
+  {
+    "revision": "32a44a573bce1dd99f78de2c5b740168",
+    "url": "/static/media/Starfish.32a44a57.svg"
+  },
+  {
+    "revision": "c073e5fc1499abffbf104d8bd9f73a87",
+    "url": "/static/media/VenusResume.c073e5fc.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.441beaf535ce2ea5c0a43384e687a813.js"
+  "/precache-manifest.8ce9b9be091dd178f058f8639811060b.js"
 );
 
 self.addEventListener('message', (event) => {
